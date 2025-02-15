d472ec90ef10ed5f8aa2b397a479b28053a293e6
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-15 11:46:52 -0300

    creo enrutador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
