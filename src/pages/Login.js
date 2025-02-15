72d191802a2bb147559cd52c9af702e4f927d206
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-15 10:42:57 -0300

    boton send formulario

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,14 +3,19 @@ import {useState} from 'react'
 function Login() {
   const [email, setEmail] = useState('');
 
+  const handleSumit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSumit}>
         <input
           type="email"
           name="email"
           placeholder="youremail@site.com"
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <button>Send</button>
       </form>
