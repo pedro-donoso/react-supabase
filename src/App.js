9b056f08ac2b73bc7f4f1fae6ca5c95a9db1c452
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-15 12:07:40 -0300

    creo pagina not found

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from 'react-router-dom'
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
